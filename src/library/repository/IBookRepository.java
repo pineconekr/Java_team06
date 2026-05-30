cfb5b62ab56873c864dc0d17fd2438dc6439d402
pineconekr <solbin2255@naver.com>
2026-05-30 14:17:59 +0900

    복구

--- src/library/repository/IBookRepository.java
+++ src/library/repository/IBookRepository.java
@@ -8,6 +8,5 @@ public interface IBookRepository {
     void add(Book book);
     boolean delete(String isbn);
     Optional<Book> findByIsbn(String isbn);
-     boolean delete(String isbn); //고친코드
     List<Book> findAll();
 }
